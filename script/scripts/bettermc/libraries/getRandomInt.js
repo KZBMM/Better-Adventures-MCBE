38c8ce971afd98df892577cd4a3dbcb26b68ef77
KZBMM <131531916+KZBMM@users.noreply.github.com>
2023-07-04 10:59:58 +0200

    Delete script directory

--- script/scripts/bettermc/libraries/getRandomInt.js
+++
@@ -1,8 +0,0 @@
-
-import * as minecraft_main_server from '@minecraft/server';
-
-//Simple Library
-
-export function getRandomInt(min, max) {
-	return Math.round(Math.random() * (max - min)) + min;
-}
