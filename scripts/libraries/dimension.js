bc9e329ea0eade764f7bb148ad788b97dcb9572e
KZBMM <131531916+KZBMM@users.noreply.github.com>
2024-01-16 18:09:32 +0100

    Delete scripts directory

--- scripts/libraries/dimension.js
+++
@@ -1,7 +0,0 @@
-
-import * as minecraft_main_server from '@minecraft/server';
-
-//Simple Library
-
-export let overworld = minecraft_main_server.world.getDimension("overworld");
-export let nether = minecraft_main_server.world.getDimension("nether");
--- scripts/libraries/getRandomInt.js
+++
@@ -1,9 +0,0 @@
-//Simple Library
-
-export function getRandomInt(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-export function getRandomFloat(min, max) {
-	return (Math.random() * (max - min)) + min;
-}
